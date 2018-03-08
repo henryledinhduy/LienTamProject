78b9dbdba5375ea98daf319ff94ca0f555207a14
henryledinhduy <henryledinhduy@gmail.com>
2018-03-09 00:14:27 +0200

    Added Demo Register From - not link to database yet

--- lientamcms/membership/admin.py
+++ lientamcms/membership/admin.py
@@ -3,3 +3,4 @@ from .models import People, Donation
 
 admin.site.register(People)
 admin.site.register(Donation)
+#admin.site.register(Contact)
--- lientamcms/membership/models.py
+++ lientamcms/membership/models.py
@@ -1,11 +1,29 @@
 from django.db import models
+from djmoney.models.fields import MoneyField
 
 class People(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=50)
-    social_number = models.CharField(max_length=14)
+    profession = models.CharField(max_length=50)
+    social_number = models.CharField(max_length=14, unique=True)
+    phap_danh = models.CharField(max_length=20)
+    nationality = models.CharField(max_length=20)
+    gender = models.CharField(max_length=10)
 
 class Donation(models.Model):
     people = models.ForeignKey(People, on_delete=models.CASCADE)
     donate_date = models.DateTimeField('date donated')
     amount = models.IntegerField(default=0)
+
+class Contact(models.Model):
+    people = models.ForeignKey(People, on_delete=models.CASCADE)
+    address = models.CharField(max_length=100)
+    postal_code = models.CharField(max_length=10)
+    city = models.CharField(max_length=10)
+    phone = models.CharField(max_length=15)
+    email = models.CharField(max_length=50)
+
+class Setting(models.Model):
+    people = models.ForeignKey(People, on_delete=models.CASCADE)
+    email_receive = models.BooleanField(default=False)
+    phone_receive = models.BooleanField(default=False)
